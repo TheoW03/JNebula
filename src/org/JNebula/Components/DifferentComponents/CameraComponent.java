507b203cd18fd1ea1cd46cde0e54bd51753d87ee
Theo <95878514+TheoW03@users.noreply.github.com>
2023-07-13 18:05:46 -0400

    :3

--- src/org/JNebula/Components/DifferentComponents/CameraComponent.java
+++ src/org/JNebula/Components/DifferentComponents/CameraComponent.java
@@ -16,23 +16,23 @@ public class CameraComponent extends Component {
 
     public Camera camera;
     public Vector3 cameralocation;
-    public CameraComponent(Vector3 cameraLocation){
+
+    public CameraComponent(Vector3 cameraLocation) {
         this.camera = new Camera(cameraLocation);
         this.cameralocation = cameraLocation;
+    }
+
+    public void zoom(float zoomfactor) {
+        this.camera.zoom(zoomfactor);
+
     }
 
     @Override
     public void sendtoGPU(int shaderProgram, Shader sh) {
         int matricesLocation = gl.glGetUniformLocation(shaderProgram, "viewMatrix");
         int projectionLocation = gl.glGetUniformLocation(shaderProgram, "projectMatrix");
-        sh.sendMatrices(camera.viewMatrix(),gl,matricesLocation);
-        sh.sendMatrices(camera.getProjection(),gl,projectionLocation);
+        sh.sendMatrices(camera.viewMatrix(), gl, matricesLocation);
+        sh.sendMatrices(camera.getProjection(), gl, projectionLocation);
     }
-    public void zoom(float zoomfactor){
-        this.camera.zoom(zoomfactor);
-    }
-    @Override
-    public void update(float dt) {
 
-    }
 }
