16f4772f4f148b8ccb7c316818ba731f53ca6d8b
Theo <95878514+TheoW03@users.noreply.github.com>
2023-07-22 19:59:38 -0400

    :3

---
+++ src/InfinityTrain/InfinityTRainRender.java
@@ -0,0 +1,18 @@
+package InfinityTrain;
+
+import org.JNebula.GameObjects.GameRenderer;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * @author Theo willis
+ * @version 1.0.0
+ * ~ project outline here ~
+ * @Javadoc
+ */
+public class InfinityTRainRender extends GameRenderer {
+
+
+
+}
---
+++ src/InfinityTrain/InfinityTrain.java
@@ -0,0 +1,23 @@
+package InfinityTrain;
+
+import DemoGame.DemoRenderer;
+import TestStuff.RendererTest2D;
+import org.JNebula.Tooling.Window;
+
+import java.util.*;
+import java.io.*;
+
+
+/**
+ * @author Theo willis
+ * @version 1.0.0
+ * ~ project outline here ~
+ * @Javadoc
+ */
+public class InfinityTrain {
+
+
+    public static void main(String[] args) {
+        Window.InitWindow(640, 480, "JNebula", new DemoRenderer());
+    }
+}
