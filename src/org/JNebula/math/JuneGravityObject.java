53da1467c4d23bafe7ef4bffe4cad12abcc178fb
Juniper <86971433+JuneDeveloper@users.noreply.github.com>
2023-08-23 19:08:43 +0100

    Added comments

--- src/org/JNebula/math/JuneGravityObject.java
+++ src/org/JNebula/math/JuneGravityObject.java
@@ -2,7 +2,8 @@ package org.JNebula.math;
 
 import org.joml.Vector3f;
 
-public class JuneGravityObject {
+public class JuneGravityObject { // :3, gravity comes from june's brain because it is very dense
+    public static final float G = 6.67f; // 6.67f is realistic, but can be changed for game purposes if needed
     float mass;
     float density;
     float size;
@@ -15,10 +16,10 @@ public class JuneGravityObject {
         this.density = density;
     }
 
-    public Vector3f getGravity(JuneGravityObject[] objects) {
+    public Vector3f getGravity(JuneGravityObject[] objects) { // Calculates the force of all the relevant gravity objects in the scene
         Vector3f totalForce = new Vector3f(0, 0, 0);
         for (JuneGravityObject object : objects) {
-            float force = (6.67f * object.mass * this.mass * (this.position.length() - object.position.length())) * (size / density);
+            float force = (G * object.mass * this.mass * (this.position.length() - object.position.length())) * (size / density);
             Vector3f heading = object.position.sub(this.position);
             Vector3f forceHeading = new Vector3f(
                     heading.x * force,
@@ -27,6 +28,6 @@ public class JuneGravityObject {
             );
             totalForce.add(forceHeading);
         }
-        return totalForce;
+        return totalForce; // Returns the force, this should be added to the velocity of the target object
     }
 }
